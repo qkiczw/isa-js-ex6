94d135bf1c9c93b4758636f34c3d74daa4694097
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2017-12-02 18:32:19 +0100

    Ex6, point 01 -done

--- exercise6.js
+++ exercise6.js
@@ -1,12 +1,13 @@
-// Funcrtiom 01
+// point 01
 var sender = 'łukasz',
     receiver = 'łukasz',
-    sms = 'Cześć Łukasz. Czy ta funkcja działa prawidłowo?',
+    sms = 'Cześć Łukasz. Czy ta funkcja działa prawidłowo? ',
     from = 'from: ',
     to = 'to: ';
 
 sender = sender.charAt(0).toUpperCase() + sender.substring(1).toLowerCase();
 receiver = receiver.charAt(0).toUpperCase() + sender.substring(1).toLowerCase();
+sms.trim();
 from = from.toUpperCase();
 to = to.toUpperCase();
 
@@ -18,3 +19,5 @@ function message(sender, sms, receiver) {
 console.log(message(sender, sms, receiver));
 
 // "(FROM: sender) (TO: receiver) (MESSAGE: sms)"
+
+
