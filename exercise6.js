40b78c99a951369da8d1e15a2a5ecfe1dcc7491e
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2017-12-02 18:21:53 +0100

    First commit - add ex1 in exercise6.js

---
+++ exercise6.js
@@ -0,0 +1,20 @@
+// Funcrtiom 01
+var sender = 'łukasz',
+    receiver = 'łukasz',
+    sms = 'Cześć Łukasz. Czy ta funkcja działa prawidłowo?',
+    from = 'from: ',
+    to = 'to: ';
+
+sender = sender.charAt(0).toUpperCase() + sender.substring(1).toLowerCase();
+receiver = receiver.charAt(0).toUpperCase() + sender.substring(1).toLowerCase();
+from = from.toUpperCase();
+to = to.toUpperCase();
+
+
+function message(sender, sms, receiver) {
+    return '(' + from + sender + ') (' + to + receiver + ') (' + sms + ')';
+}
+
+console.log(message(sender, sms, receiver));
+
+// "(FROM: sender) (TO: receiver) (MESSAGE: sms)"
